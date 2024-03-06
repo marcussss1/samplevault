6557f3fddca5b347f86e9c069c7a2b9de1b451be
litvak.danila <litvak.danila@wb.ru>
2024-03-06 20:19:09 +0300

    edit configs

--- update_deployment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Заменяем значение image в deployment.yml на сгенерированную версию образа
-sed -i "s|image: marcussss/simplevault:.*|image: marcussss/simplevault:$(echo $GITHUB_SHA | cut -c -7)|" deployment.yml
