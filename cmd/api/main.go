1b97691eab889e64f6d4069b9382e5fc99b6deb0
litvak.danila <litvak.danila@wb.ru>
2024-03-07 21:32:21 +0300

    edit logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,7 @@ func main() {
 		return ctx.JSON(http.StatusOK, struct {
 			Message string `json:"message"`
 		}{
-			Message: "SIMPLE VAULT VERSION 10",
+			Message: "SIMPLE VAULT VERSION 11",
 		})
 	})
 
