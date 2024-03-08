60d2b9ce8e852fe9639a51d533ae2cc51be9f4b0
litvak.danila <litvak.danila@wb.ru>
2024-03-08 13:29:44 +0300

    edit logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -14,6 +14,14 @@ func main() {
 		AllowCredentials: false,
 	}))
 
+	e.GET("/", func(ctx echo.Context) error {
+		return ctx.JSON(http.StatusOK, struct {
+			Message string `json:"message"`
+		}{
+			Message: "SIMPLE VAULT VERSION 230",
+		})
+	})
+
 	e.GET("/api", func(ctx echo.Context) error {
 		return ctx.JSON(http.StatusOK, struct {
 			Message string `json:"message"`
