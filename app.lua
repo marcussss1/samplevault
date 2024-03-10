db186734e841d6a98e00e32c80ae77bc29c50280
litvak.danila <litvak.danila@wb.ru>
2024-03-10 14:10:06 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -18,18 +18,18 @@ box.cfg {
 
 
 
-box.schema.space.create('bands', {if_not_exists = true,engine='vinyl'})
-
-box.space.bands:format({
-    { name = 'id', type = 'unsigned' },
-    { name = 'band_name', type = 'string' },
-    { name = 'year', type = 'unsigned' }
-})
-
-box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
-
-box.space.bands:insert { 1, 'Roxette', 1986 }
-box.space.bands:insert { 2, 'Scorpions', 1965 }
-box.space.bands:insert { 3, 'Ace of Base', 1987 }
+--box.schema.space.create('bands', {if_not_exists = true,engine='vinyl'})
+--
+--box.space.bands:format({
+--    { name = 'id', type = 'unsigned' },
+--    { name = 'band_name', type = 'string' },
+--    { name = 'year', type = 'unsigned' }
+--})
+--
+--box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+--
+--box.space.bands:insert { 1, 'Roxette', 1986 }
+--box.space.bands:insert { 2, 'Scorpions', 1965 }
+--box.space.bands:insert { 3, 'Ace of Base', 1987 }
 
 --box.space.bands:select { 3 }
