d2877f0f2805082fe10bf9b8a6052366adc19fa2
litvak.danila <litvak.danila@wb.ru>
2024-03-10 14:34:56 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -2,4 +2,20 @@ box.cfg {
     listen = 3301,
     vinyl_dir = '/var/lib/tarantool',
     wal_mode = 'write',
+    checkpoint_count = 5,
+    checkpoint_interval = 3600,
 }
+
+box.schema.space.create('bands', { engine = 'vinyl' })
+
+box.space.bands:format({
+    { name = 'id', type = 'unsigned' },
+    { name = 'band_name', type = 'string' },
+    { name = 'year', type = 'unsigned' }
+})
+
+box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+
+box.space.bands:insert { 1, 'Roxette', 1986 }
+box.space.bands:insert { 2, 'Scorpions', 1965 }
+box.space.bands:insert { 3, 'Ace of Base', 1987 }
