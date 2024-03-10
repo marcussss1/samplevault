1c6804aaa3ae140c3afb394119e4bbec1e9fe5c4
litvak.danila <litvak.danila@wb.ru>
2024-03-10 14:08:14 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -2,7 +2,6 @@ box.cfg {
     listen = 3301,
     vinyl_dir = '/var/lib/tarantool',
     wal_mode = 'none',
-    vinyl = true,
 }
 
 --vinyl.storage.engine = "vinyl"
@@ -19,18 +18,18 @@ box.cfg {
 
 
 
---box.schema.space.create('bands', {if_not_exists = true})
---
---box.space.bands:format({
---    { name = 'id', type = 'unsigned' },
---    { name = 'band_name', type = 'string' },
---    { name = 'year', type = 'unsigned' }
---})
---
---box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
---
---box.space.bands:insert { 1, 'Roxette', 1986 }
---box.space.bands:insert { 2, 'Scorpions', 1965 }
---box.space.bands:insert { 3, 'Ace of Base', 1987 }
+box.schema.space.create('bands', {if_not_exists = true,engine='vinyl'})
+
+box.space.bands:format({
+    { name = 'id', type = 'unsigned' },
+    { name = 'band_name', type = 'string' },
+    { name = 'year', type = 'unsigned' }
+})
+
+box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+
+box.space.bands:insert { 1, 'Roxette', 1986 }
+box.space.bands:insert { 2, 'Scorpions', 1965 }
+box.space.bands:insert { 3, 'Ace of Base', 1987 }
 
 --box.space.bands:select { 3 }
