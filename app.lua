957d47375773636c87e4787c0eb9130143cc6d93
litvak.danila <litvak.danila@wb.ru>
2024-03-10 12:13:12 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -4,6 +4,10 @@ local instances = {
         instance_uuid = "b713e340-6df3-44c7-b55b-dbc9413c46f2",
         replicaset_uuid = "1437b89c-d73f-4612-be07-e59b566f09f1"
     },
+    ["tarantool-1"] = {
+        instance_uuid = "0e44fbf7-ebf9-45e0-9672-aeaaf09c508e",
+        replicaset_uuid = "1437b89c-d73f-4612-be07-e59b566f09f1"
+    },
 }
 
 assert(instance_name)
