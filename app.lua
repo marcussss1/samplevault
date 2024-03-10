2e0db160d14357b0192d816541f813df5079de10
litvak.danila <litvak.danila@wb.ru>
2024-03-10 13:07:22 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -1,14 +1,31 @@
-box.cfg {
-    listen = 3301,
-    vinyl_dir = '/tmp/tarantool',
+--box.cfg {
+--    listen = 3301,
+--    vinyl_dir = '/tmp/tarantool',
+--}
+--
+--box.schema.space.create('bands')
+--
+--box.space.bands:format({
+--    { name = 'id', type = 'unsigned' },
+--    { name = 'band_name', type = 'string' },
+--    { name = 'year', type = 'unsigned' }
+--})
+--
+--box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+
+local instance_name = os.environ()["TARANTOOL_INSTANCE_NAME"]
+local instances = {
+    ["tarantool-0"] = {
+        instance_uuid = "b713e340-6df3-44c7-b55b-dbc9413c46f2",
+        replicaset_uuid = "1437b89c-d73f-4612-be07-e59b566f09f1"
+    },
+    ["tarantool-1"] = {
+        instance_uuid = "0e44fbf7-ebf9-45e0-9672-aeaaf09c508e",
+        replicaset_uuid = "1437b89c-d73f-4612-be07-e59b566f09f1"
+    },
 }
 
-box.schema.space.create('bands')
+assert(instance_name)
+assert(instances[instance_name])
 
-box.space.bands:format({
-    { name = 'id', type = 'unsigned' },
-    { name = 'band_name', type = 'string' },
-    { name = 'year', type = 'unsigned' }
-})
-
-box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+box.cfg(instances[instance_name])
