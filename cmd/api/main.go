3967bac8a40f64eefc4683b9dddd075527298986
litvak.danila <litvak.danila@wb.ru>
2024-03-10 16:48:38 +0300

    add tarantool

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/tarantool/go-tarantool"
 	"net/http"
 )
 
@@ -15,25 +16,26 @@ func main() {
 		AllowCredentials: false,
 	}))
 
-	//conn, err := tarantool.Connect("tarantool-0.tarantool.default.svc.cluster.local:3301", tarantool.Opts{
-	//	User: "tarantool",
-	//	Pass: "tarantool",
-	//})
-	//if err != nil {
-	//	log.Fatal("Connection refused")
-	//}
-	//defer conn.Close()
+	conn, err := tarantool.Connect("tarantool-0.tarantool.default.svc.cluster.local:3301", tarantool.Opts{
+		User: "tarantool",
+		Pass: "tarantool",
+	})
+	if err != nil {
+		fmt.Println("Connection refused")
+		return
+	}
+	defer conn.Close()
 
 	e.GET("/api/v1", func(ctx echo.Context) error {
-		//resp, err := conn.Select("tarantool", "primary", 0, 1, tarantool.IterEq, []interface{}{3})
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
-		//
-		//fmt.Println(resp.Data)
-		//log.Fatal(1)
+		resp, err := conn.Select("tarantool", "primary", 0, 1, tarantool.IterEq, []interface{}{3})
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
 
+		fmt.Println(resp)
 		fmt.Println("VERSION 2 REQUEST")
+
 		return ctx.JSON(http.StatusOK, struct {
 			Message string `json:"message"`
 		}{
