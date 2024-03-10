4fe2997c81ed7b97f51df1ec80b5e0c01d6c2eab
litvak.danila <litvak.danila@wb.ru>
2024-03-10 13:14:08 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -1,18 +1,3 @@
---box.cfg {
---    listen = 3301,
---    vinyl_dir = '/tmp/tarantool',
---}
---
---box.schema.space.create('bands')
---
---box.space.bands:format({
---    { name = 'id', type = 'unsigned' },
---    { name = 'band_name', type = 'string' },
---    { name = 'year', type = 'unsigned' }
---})
---
---box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
-
 local instance_name = os.environ()["TARANTOOL_INSTANCE_NAME"]
 local instances = {
     ["tarantool-0"] = {
---
+++ tar.lua
@@ -0,0 +1,20 @@
+box.cfg {
+    listen = 3301,
+    vinyl_dir = '/tmp/tarantool',
+}
+
+box.schema.space.create('bands')
+
+box.space.bands:format({
+    { name = 'id', type = 'unsigned' },
+    { name = 'band_name', type = 'string' },
+    { name = 'year', type = 'unsigned' }
+})
+
+box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
+
+box.space.bands:insert { 1, 'Roxette', 1986 }
+box.space.bands:insert { 2, 'Scorpions', 1965 }
+box.space.bands:insert { 3, 'Ace of Base', 1987 }
+
+--box.space.bands:select { 3 }
