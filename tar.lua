876ab8a5ec7b5953373d090562bca7d6f647df8f
litvak.danila <litvak.danila@wb.ru>
2024-03-10 11:27:48 +0300

    add tarantool

---
+++ tar.lua
@@ -0,0 +1,7 @@
+box.cfg {
+    listen = 3301,
+    vinyl_dir = '/var/lib/tarantool',
+}
+
+box.schema.space.create('my_space', {if_not_exists = true})
+box.space.my_space:create_index('primary', {type = 'tree', parts = {1, 'unsigned'}})
