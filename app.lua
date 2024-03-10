ee62bd3c58aeba8101d9213c729900a9d1ef1b91
litvak.danila <litvak.danila@wb.ru>
2024-03-10 16:13:13 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -12,7 +12,7 @@ box.cfg {
 
 box.schema.user.create(user, { password = password, if_not_exists = true })
 box.schema.create_space(database, { if_not_exists = true })
-box.schema.user.grant(user,'read,write,execute,create,drop','universe',{ if_not_exists = true })
+box.schema.user.grant(user, 'read,write,execute,create,drop', 'universe', nil, { if_not_exists = true })
 
 -- Print confirmation message
 print('Database initialized')
