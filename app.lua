4b95f4a742b63c53af91f4e6962b4acec2540465
litvak.danila <litvak.danila@wb.ru>
2024-03-10 13:00:25 +0300

    add tarantool

--- app.lua
+++ app.lua
@@ -3,6 +3,12 @@ box.cfg {
     vinyl_dir = '/tmp/tarantool',
 }
 
-box.schema.space.create('samplevault', {if_not_exists = true})
-box.space.my_space:create_index('primary', {type = 'tree', parts = {1, 'unsigned'}})
+box.schema.space.create('bands')
 
+box.space.bands:format({
+    { name = 'id', type = 'unsigned' },
+    { name = 'band_name', type = 'string' },
+    { name = 'year', type = 'unsigned' }
+})
+
+box.space.bands:create_index('primary', { type = "tree", parts = { 'id' } })
