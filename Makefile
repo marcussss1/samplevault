bec7b22616b30820c8262947ac066fba554c4cc0
litvak.danila <litvak.danila@wb.ru>
2024-03-11 20:08:58 +0300

    add tarantool

--- Makefile
+++ Makefile
@@ -1,9 +1,3 @@
-generate_image_version:
-    @image_version=$$(openssl rand -base64 12); \
-    export IMAGE_VERSION=$$image_version;
-
-.PHONY: generate_image_version
-
 start: |
 	docker build -t "samplevault:$$IMAGE_VERSION" -f "Dockerfile" .
 	kubectl set image deployments/samplevault samplevault=samplevault:$$IMAGE_VERSION
---
+++ generate_image_version.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+image_version=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+export IMAGE_VERSION="$image_version"
+echo "Версия образа: $IMAGE_VERSION"
+eval $(minikube docker-env)
+docker build -t "samplevault:$$IMAGE_VERSION" -f "Dockerfile" .
+kubectl set image deployments/samplevault samplevault=samplevault:$$IMAGE_VERSION
+kubectl rollout status deployments/samplevault
+#docker rmi -f samplevault:$$IMAGE_VERSION
+eval $(minikube docker-env -u)
