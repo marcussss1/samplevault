57dc6d1ce9881f483454ead2f80928b2cbcde252
litvak.danila <litvak.danila@wb.ru>
2024-03-19 20:31:35 +0300

    add logging

--- internal/controller/http/samplesv1/get_all_samples.go
+++ internal/controller/http/samplesv1/get_all_samples.go
@@ -11,6 +11,7 @@ func (c Controller) GetAllSamples(ctx echo.Context) error {
 	// TODO Временно, когда появится авторизация нужно поменять логику
 	req := ctx.Request()
 	userID := ctx.Get("session_id").(string)
+	fmt.Println("userID: ", userID)
 	samples, err := c.samplesService.GetAllSamples(req.Context(), userID)
 	if err != nil {
 		return fmt.Errorf("get all samples from samples service: %w", err)
--- internal/controller/http/samplesv1/upload_sample.go
+++ internal/controller/http/samplesv1/upload_sample.go
@@ -11,7 +11,7 @@ func (c Controller) UploadSample(ctx echo.Context) error {
 	req := ctx.Request()
 	// TODO Временно, когда появится авторизация нужно поменять логику
 	userID := ctx.Get("session_id").(string)
-
+	fmt.Println("userID: ", userID)
 	err := req.ParseMultipartForm(5 * 1024 * 1024)
 	if err != nil {
 		return fmt.Errorf("parse multipart form: %w", err)
