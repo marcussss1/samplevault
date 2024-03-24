16191901f70171c26b9eea44d8853b8df9ff016f
litvak.danila <litvak.danila@wb.ru>
2024-03-24 13:14:36 +0300

    edit create sound logic

--- internal/infrastructure/tarantool/sounds/store_sound.go
+++ internal/infrastructure/tarantool/sounds/store_sound.go
@@ -2,37 +2,57 @@ package sounds
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/marcussss1/simplevault/internal/model"
 )
 
 func (r Repository) StoreSound(ctx context.Context, sound model.Sound) error {
-	data := map[string]interface{}{
-		"id":                 sound.ID,
-		"author_id":          sound.AuthorID,
-		"audio_url":          sound.AudioURL,
-		"icon_url":           sound.IconURL,
-		"file_name":          sound.FileName,
-		"created_at":         sound.CreatedAt,
-		"title":              sound.Title,
-		"musical_instrument": sound.MusicalInstrument,
-		"genre":              sound.Genre,
-		"mood":               sound.Mood,
-		"tonality":           sound.Tonality,
-		"tempo":              sound.Tempo,
-		"style":              sound.Style,
-	}
+	_, err := r.conn.Eval(`
+    function escapeString(str)
+        return str:gsub('[%-%.%+%[%]%(%)%*%?%^%$%%]', '%%%1')
+    end
 
-	packedData, err := json.Marshal(data)
+    function insertSound(sound)
+        local escapedData = {}
+        for key, value in pairs(sound) do
+            if type(value) == "string" then
+                escapedData[key] = escapeString(value)
+            else
+                escapedData[key] = value
+            end
+        end
+        
+        box.space.sounds:insert(escapedData)
+    end
+`, nil)
 	if err != nil {
-		return fmt.Errorf("error packing data: %w", err)
+		return fmt.Errorf("error defining Lua functions: %w", err)
 	}
 
-	_, err = r.conn.Insert("sounds", packedData)
+	_, err = r.conn.Call("insertSound", sound)
 	if err != nil {
 		return fmt.Errorf("insert sound from tarantool storage: %w", err)
 	}
 
 	return nil
+	//_, err := r.conn.Insert("sounds", []interface{}{
+	//	sound.ID,
+	//	sound.AuthorID,
+	//	sound.AudioURL,
+	//	sound.IconURL,
+	//	sound.FileName,
+	//	sound.CreatedAt,
+	//	sound.Title,
+	//	sound.MusicalInstrument,
+	//	sound.Genre,
+	//	sound.Mood,
+	//	sound.Tonality,
+	//	sound.Tempo,
+	//	sound.Style,
+	//})
+	//if err != nil {
+	//	return fmt.Errorf("insert sound from tarantool storage: %w", err)
+	//}
+
+	//return nil
 }
