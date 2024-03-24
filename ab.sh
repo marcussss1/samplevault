87eca5330a48ed8a641b81c3d6f6fd49d86ffcf4
litvak.danila <litvak.danila@wb.ru>
2024-03-24 11:56:49 +0300

    edit create sound logic

--- ab.sh
+++ ab.sh
@@ -3,8 +3,7 @@
 API_URL="https://samplevault.ru/api/v1/sounds/upload"
 
 for file in *; do
-    if [ -f "$file" ]; then
-      echo "audio=@$file"
+    if [ -f "$file" ] && [ "$file" != "ab.sh" ]; then
         curl -F "audio=@$file" $API_URL -v
     fi
 done
--- internal/service/audio/upload_sound.go
+++ internal/service/audio/upload_sound.go
@@ -7,19 +7,21 @@ import (
 	"github.com/marcussss1/simplevault/internal/model"
 	"mime/multipart"
 	"net/url"
+	"path/filepath"
 	"time"
 )
 
 func (s Service) UploadSound(ctx context.Context, file multipart.File, header *multipart.FileHeader, userID string) (model.Sound, error) {
-	//extension := filepath.Ext(header.Filename)
-	//filename := uuid.NewString() + extension
+	id := uuid.NewString()
+	extension := filepath.Ext(header.Filename)
+	filename := id + extension
 
-	url, err := s.minioRepository.UploadSound(ctx, file, header.Filename, header.Size)
+	url, err := s.minioRepository.UploadSound(ctx, file, filename, header.Size)
 	if err != nil {
 		return model.Sound{}, fmt.Errorf("upload sound from minio repository: %w", err)
 	}
 
-	sample := newSample(url, userID, header.Filename)
+	sample := newSample(url, id, userID, filename)
 
 	err = s.tarantoolRepository.StoreSound(ctx, sample)
 	if err != nil {
@@ -29,12 +31,12 @@ func (s Service) UploadSound(ctx context.Context, file multipart.File, header *m
 	return sample, nil
 }
 
-func newSample(url *url.URL, userID, filename string) model.Sound {
+func newSample(url *url.URL, id, userID, filename string) model.Sound {
 	url.Scheme = "https"
 	url.Host = "samplevault.ru"
 
 	return model.Sound{
-		ID:                uuid.NewString(),
+		ID:                id,
 		AuthorID:          userID,
 		AudioURL:          url.String(),
 		IconURL:           "https://img.freepik.com/free-photo/the-adorable-illustration-of-kittens-playing-in-the-forest-generative-ai_260559-483.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1710892800&semt=ais",
