523152c57d0f36e06647476acd355e44f6dbc356
litvak.danila <litvak.danila@wb.ru>
2024-03-28 19:26:20 +0300

    add generate sound logic

--- internal/infrastructure/tarantool/sounds/get_random_sounds.go
+++ internal/infrastructure/tarantool/sounds/get_random_sounds.go
@@ -15,22 +15,27 @@ func (r Repository) GetRandomSounds(ctx context.Context) ([]model.Sound, error) 
 	}
 
 	for _, tuple := range resp.Tuples() {
-		sounds = append(sounds, model.Sound{
-			ID:                fmt.Sprint(tuple[0]),
-			AuthorID:          fmt.Sprint(tuple[1]),
-			AudioURL:          fmt.Sprint(tuple[2]),
-			IconURL:           fmt.Sprint(tuple[3]),
-			FileName:          fmt.Sprint(tuple[4]),
-			CreatedAt:         fmt.Sprint(tuple[5]),
-			Title:             fmt.Sprint(tuple[6]),
-			MusicalInstrument: fmt.Sprint(tuple[7]),
-			Genre:             fmt.Sprint(tuple[8]),
-			Mood:              fmt.Sprint(tuple[9]),
-			Tonality:          fmt.Sprint(tuple[10]),
-			Tempo:             fmt.Sprint(tuple[11]),
-			Style:             fmt.Sprint(tuple[12]),
-			IsGenerated:       false,
-		})
+		for _, a := range tuple {
+			//fmt.Println(a[0])
+			b := a.(model.Sound)
+			fmt.Println(b)
+		}
+		//sounds = append(sounds, model.Sound{
+		//	ID:                fmt.Sprint(tuple[0]),
+		//	AuthorID:          fmt.Sprint(tuple[1]),
+		//	AudioURL:          fmt.Sprint(tuple[2]),
+		//	IconURL:           fmt.Sprint(tuple[3]),
+		//	FileName:          fmt.Sprint(tuple[4]),
+		//	CreatedAt:         fmt.Sprint(tuple[5]),
+		//	Title:             fmt.Sprint(tuple[6]),
+		//	MusicalInstrument: fmt.Sprint(tuple[7]),
+		//	Genre:             fmt.Sprint(tuple[8]),
+		//	Mood:              fmt.Sprint(tuple[9]),
+		//	Tonality:          fmt.Sprint(tuple[10]),
+		//	Tempo:             fmt.Sprint(tuple[11]),
+		//	Style:             fmt.Sprint(tuple[12]),
+		//	IsGenerated:       false,
+		//})
 	}
 
 	return sounds, nil
