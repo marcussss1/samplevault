46fe873ea947056228c338a74c67b6b5ef5acdbc
litvak.danila <litvak.danila@wb.ru>
2024-03-28 18:48:43 +0300

    add generate sound logic

---
+++ internal/infrastructure/tarantool/sounds/get_random_sounds.go
@@ -0,0 +1,29 @@
+package sounds
+
+import (
+	"context"
+	"fmt"
+	"github.com/marcussss1/simplevault/internal/model"
+)
+
+func (r Repository) GetRandomSounds(ctx context.Context) ([]model.Sound, error) {
+
+	resp, err := r.conn.Eval("return 42", []interface{}{})
+	if err != nil {
+		return nil, fmt.Errorf("select all sounds from tarantool storage: %w", err)
+	}
+
+	fmt.Println(resp.Data[0].([]interface{})[0].(uint64))
+	//var sounds []model.Sound
+	//
+	//r.conn.Eval("", interface{}{})
+	//
+	//err := r.conn.SelectTyped("sounds", "primary", 0, 50,
+	//	tarantool.IterAll, tarantool.StringKey{""}, &sounds,
+	//)
+	//if err != nil {
+	//	return nil, fmt.Errorf("select all sounds from tarantool storage: %w", err)
+	//}
+	//
+	//return sounds, nil
+}
