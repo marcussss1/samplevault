e71260021a666fbc784a24b24b49edd7bc594b10
litvak.danila <litvak.danila@wb.ru>
2024-03-28 19:29:37 +0300

    add generate sound logic

--- internal/infrastructure/tarantool/sounds/get_random_sounds.go
+++ internal/infrastructure/tarantool/sounds/get_random_sounds.go
@@ -14,11 +14,29 @@ func (r Repository) GetRandomSounds(ctx context.Context) ([]model.Sound, error) 
 		return nil, fmt.Errorf("select all sounds from tarantool storage: %w", err)
 	}
 
-	for _, tuple := range resp.Tuples() {
-		for _, a := range tuple {
-			//fmt.Println(a[0])
-			b := a.(model.Sound)
-			fmt.Println(b)
+	for _, tuples := range resp.Tuples() {
+		for _, tuple := range tuples {
+			switch t := tuple.(type) {
+			case []interface{}:
+				sounds = append(sounds, model.Sound{
+					ID:                fmt.Sprint(t[0]),
+					AuthorID:          fmt.Sprint(t[1]),
+					AudioURL:          fmt.Sprint(t[2]),
+					IconURL:           fmt.Sprint(t[3]),
+					FileName:          fmt.Sprint(t[4]),
+					CreatedAt:         fmt.Sprint(t[5]),
+					Title:             fmt.Sprint(t[6]),
+					MusicalInstrument: fmt.Sprint(t[7]),
+					Genre:             fmt.Sprint(t[8]),
+					Mood:              fmt.Sprint(t[9]),
+					Tonality:          fmt.Sprint(t[10]),
+					Tempo:             fmt.Sprint(t[11]),
+					Style:             fmt.Sprint(t[12]),
+					IsGenerated:       false,
+				})
+			default:
+				panic(1)
+			}
 		}
 		//sounds = append(sounds, model.Sound{
 		//	ID:                fmt.Sprint(tuple[0]),
