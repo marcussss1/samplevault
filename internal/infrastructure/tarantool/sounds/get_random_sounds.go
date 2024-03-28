4c513939a27d264064b4d1bced76ec6dceeec6c3
litvak.danila <litvak.danila@wb.ru>
2024-03-28 19:40:17 +0300

    add generate sound logic

--- internal/infrastructure/tarantool/sounds/get_random_sounds.go
+++ internal/infrastructure/tarantool/sounds/get_random_sounds.go
@@ -9,9 +9,23 @@ import (
 func (r Repository) GetRandomSounds(ctx context.Context) ([]model.Sound, error) {
 	var sounds []model.Sound
 
-	resp, err := r.conn.Eval("return box.space.sounds:select()", []interface{}{})
+	resp, err := r.conn.Eval(`
+		local count = 0
+		local random_sounds = {}
+		
+		for _, tuple in box.space.sounds.index.primary:pairs() do
+			if count < 5 then
+				table.insert(random_sounds, tuple)
+				count = count + 1
+			else
+				break
+			end
+		end
+		
+		return random_sounds
+	`, []interface{}{})
 	if err != nil {
-		return nil, fmt.Errorf("select all sounds from tarantool storage: %w", err)
+		return nil, fmt.Errorf("select random sounds from tarantool storage: %w", err)
 	}
 
 	for _, tuples := range resp.Tuples() {
@@ -38,35 +52,7 @@ func (r Repository) GetRandomSounds(ctx context.Context) ([]model.Sound, error) 
 				panic(1)
 			}
 		}
-		//sounds = append(sounds, model.Sound{
-		//	ID:                fmt.Sprint(tuple[0]),
-		//	AuthorID:          fmt.Sprint(tuple[1]),
-		//	AudioURL:          fmt.Sprint(tuple[2]),
-		//	IconURL:           fmt.Sprint(tuple[3]),
-		//	FileName:          fmt.Sprint(tuple[4]),
-		//	CreatedAt:         fmt.Sprint(tuple[5]),
-		//	Title:             fmt.Sprint(tuple[6]),
-		//	MusicalInstrument: fmt.Sprint(tuple[7]),
-		//	Genre:             fmt.Sprint(tuple[8]),
-		//	Mood:              fmt.Sprint(tuple[9]),
-		//	Tonality:          fmt.Sprint(tuple[10]),
-		//	Tempo:             fmt.Sprint(tuple[11]),
-		//	Style:             fmt.Sprint(tuple[12]),
-		//	IsGenerated:       false,
-		//})
 	}
 
 	return sounds, nil
-	//var sounds []model.Sound
-	//
-	//r.conn.Eval("", interface{}{})
-	//
-	//err := r.conn.SelectTyped("sounds", "primary", 0, 50,
-	//	tarantool.IterAll, tarantool.StringKey{""}, &sounds,
-	//)
-	//if err != nil {
-	//	return nil, fmt.Errorf("select all sounds from tarantool storage: %w", err)
-	//}
-	//
-	//return sounds, nil
 }
