d85d4a17aeb9f0cf1b4357c6a77dcd0ba1f16bb1
litvak.danila <litvak.danila@wb.ru>
2024-05-04 13:53:03 +0300

    add bucket

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"context"
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	samplescontrollerv1 "github.com/marcussss1/simplevault/internal/controller/http/samplesv1"
@@ -16,6 +17,8 @@ import (
 	"github.com/marcussss1/simplevault/pkg/minio"
 	"github.com/marcussss1/simplevault/pkg/server"
 	"github.com/marcussss1/simplevault/pkg/tarantool"
+	minio2 "github.com/minio/minio-go/v7"
+	"log"
 )
 
 type Config struct {
@@ -90,6 +93,17 @@ func Run() error {
 	samplesControllerV1, err := samplescontrollerv1.NewController(samplesService, filesService, playlistsService, authService)
 	if err != nil {
 		return fmt.Errorf("%w", err)
+	}
+
+	err = minioClient.MakeBucket(context.Background(), "sounds", minio2.MakeBucketOptions{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	policy := `{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Principal":"*","Action":["s3:GetObject"],"Resource":["arn:aws:s3::: + sounds + /*"]}]}`
+	err = minioClient.SetBucketPolicy(context.Background(), "sounds", policy)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
 	server := server.NewServer(samplesControllerV1, authService)
