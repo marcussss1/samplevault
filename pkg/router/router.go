591433133da434cde9f3a0dbdef79c83a70ad29d
litvak.danila <litvak.danila@wb.ru>
2024-05-05 11:28:14 +0300

    add logout logic

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -20,6 +20,7 @@ func Route(e *echo.Echo, ctrl *samplescontrollerv1.Controller) *echo.Echo {
 	e.GET("/api/v1/playlists/:playlist_id", ctrl.GetPlaylist)
 
 	e.GET("api/v1/auth", ctrl.Auth)
+	e.GET("api/v1/auth/logout", ctrl.Logout)
 	e.POST("api/v1/auth/signup", ctrl.Signup)
 	e.POST("api/v1/auth/login", ctrl.Login)
 
