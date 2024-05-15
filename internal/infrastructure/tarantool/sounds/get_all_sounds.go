5786766d7cdfe0cf689a8bd803d5fabd26f70630
litvak.danila <litvak.danila@wb.ru>
2024-05-15 18:51:00 +0300

    fdaf

--- internal/infrastructure/tarantool/sounds/get_all_sounds.go
+++ internal/infrastructure/tarantool/sounds/get_all_sounds.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"fmt"
 	"github.com/marcussss1/simplevault/internal/model"
-	"github.com/tarantool/go-tarantool"
 )
 
 //nolint:gomnd
 func (r Repository) GetAllSounds(ctx context.Context) ([]model.Sound, error) {
 	var sounds []model.Sound
 
-	err := r.conn.SelectTyped("sounds", "primary", 0, 50,
+	err := r.conn.SelectTyped("sounds", "primary", 0, 1000,
 		tarantool.IterAll, tarantool.StringKey{""}, &sounds,
 	)
 	if err != nil {
