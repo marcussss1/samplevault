2c8d764a837db536447311922e168b03f56e0ea6
litvak.danila <litvak.danila@wb.ru>
2024-05-16 19:51:41 +0300

    add edit and delete sound logic

--- internal/controller/http/samplesv1/edit_sound_by_id.go
+++ internal/controller/http/samplesv1/edit_sound_by_id.go
@@ -1,20 +1,32 @@
 package samplesv1
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/marcussss1/simplevault/internal/model"
+	"io"
 	"net/http"
 )
 
 func (c Controller) EditSoundByID(ctx echo.Context) error {
-	var sound model.EditSound
-	err := ctx.Bind(&sound)
+	body, err := io.ReadAll(ctx.Request().Body)
 	if err != nil {
-		return fmt.Errorf("error while binding body: %w", err)
+		return fmt.Errorf("read all request body: %w", err)
+	}
+	defer ctx.Request().Body.Close()
+
+	fmt.Println()
+	fmt.Println(string(body)) // Выводим тело ответа в формате JSON
+	fmt.Println()
+
+	var sound model.EditSound
+	err = json.Unmarshal(body, &sound)
+	if err != nil {
+		return fmt.Errorf("decode sound 1: %w", err)
 	}
 
-	fmt.Printf("edit sound: %v", sound)
+	fmt.Printf("\nedit sound: %v \n", sound)
 
 	err = c.soundsService.EditSoundByID(ctx.Request().Context(), sound)
 	if err != nil {
